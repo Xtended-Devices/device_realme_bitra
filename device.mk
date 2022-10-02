0a6cba77a64913b58015da4b193fa97169d58d1b
Andrea Canale <vivanco11.ac@gmail.com>
2022-10-02 22:44:28 +0200

    bitra: Add overlay-xtended, remove old xdroid files

--- device.mk
+++ device.mk
@@ -36,6 +36,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_ODM)/etc/sound_trigger_platform_info.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
 
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-xtended
+    
 # Overlays
 PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
